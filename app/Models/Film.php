8c56e10dddb5387c015673982f93f4e34ed0f22c
malik_babahamou <malik_babahamou@ens.univ-artois.fr>
2025-10-08 16:01:54 +0200

    Ajout de films (models/seeder/factory)

---
+++ app/Models/Film.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    /** @use HasFactory<\Database\Factories\FilmFactory> */
+    use HasFactory;
+
+    protected $casts = [
+        'note' => 'float',
+    ];
+
+    protected $fillable = [
+        'titre',
+        'date_sortie',
+        'synopsis',
+        'duree',
+        'note'
+    ];
+
+}
---
+++ database/factories/FilmFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Film>
+ */
+class FilmFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'titre' => $this->faker->catchPhrase(), // ex : “The Last Horizon”
+            'date_sortie' => $this->faker->dateTimeBetween('-40 years', 'now'),
+            'synopsis' => $this->faker->paragraph(3),
+            'duree' => $this->faker->numberBetween(80, 180),
+            'note' => $this->faker->randomFloat(1, 0, 5),
+        ];
+    }
+}
