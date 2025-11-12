63927cc6fb1344c91462d78430fe2c54c5c9b983
Flamaiche <Flamaiche@gmail.com>
2025-11-12 17:03:37 +0100

    suppression des boutons apres la merge (qui les ajoutes dans card)

--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -7,22 +7,7 @@
         <div class="w-full md:w-2/3 lg:w-1/2 border rounded-lg shadow p-6 bg-white">
             <x-film-card :film="$film" />
 
-            <div class="flex justify-center gap-4 mt-6">
-                <a href="{{ route('films.edit', $film->id) }}"
-                   class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">
-                    ✏️ Modifier
-                </a>
-
-                <form action="{{ route('films.destroy', $film->id) }}" method="POST"
-                      onsubmit="return confirm('⚠️ Voulez-vous vraiment supprimer ce film ?');">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit"
-                            class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">
-                        🗑️ Supprimer
-                    </button>
-                </form>
-
+            <div class="flex justify-center mt-6">
                 <a href="{{ route('films.index') }}"
                    class="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700">
                     ⬅️ Retour à la liste
