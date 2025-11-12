769337320582e6701ecffd0c2f9346a3e5187b79
matheo.popieul <matheo_popieul@ens.univ-artois.fr>
2025-11-12 16:35:43 +0100

    Ajout des affiches des films dans le FilmSeeder ainsi que la taille de l'affiche dans le film-card.blade(CRUD #11)

--- resources/views/components/film-card.blade.php
+++ resources/views/components/film-card.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white shadow-lg rounded-2xl p-4 hover:shadow-xl transition">
     @if($film->media)
-        <img src="{{ $film->media }}" alt="Affiche de {{ $film->titre }}" class="w-full h-64 object-cover rounded-xl mb-4">
+        <img src="{{ $film->media }}" alt="Affiche de {{ $film->titre }}" class="w-full h-auto object-contain rounded-xl mb-4">
     @endif
 
     <h2 class="text-xl font-semibold mb-2">{{ $film->titre }}</h2>
