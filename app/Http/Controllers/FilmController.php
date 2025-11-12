ac92c5f907085dd4fa4a77b6545fb3048a28b4d0
matheo.popieul <matheo_popieul@ens.univ-artois.fr>
2025-11-12 17:10:35 +0100

    Réalisation d'un message de succes quand le film a bien ete modifier

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -62,27 +62,41 @@ class FilmController extends Controller
     }
 
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
+        $success = false;
 
-        $request->validate([
-            'titre' => 'required|string|max:255',
-            'date_sortie' => 'required|date',
-            'synopsis' => 'nullable|string',
-            'duree' => 'required|integer|min:1',
-            'note' => 'nullable|numeric|min:0|max:5',
-            'media' => 'nullable|url',
+        try {
+            $request->validate([
+                'titre' => 'required|string|max:255',
+                'date_sortie' => 'required|date',
+                'synopsis' => 'nullable|string',
+                'duree' => 'required|integer|min:1',
+                'note' => 'nullable|numeric|min:0|max:5',
+                'media' => 'nullable|url',
+            ]);
+            $film = Film::findOrFail($id);
+            $film->titre = $request->input('titre');
+            $film->date_sortie = $request->input('date_sortie');
+            $film->synopsis = $request->input('synopsis');
+            $film->duree = $request->input('duree');
+            $film->note = $request->input('note');
+            $film->media = $request->input('media');
+            $film->save();
+
+            $success = true;
+            $message = "Film mis à jour avec succès !";
+        } catch (\Exception $e) {
+            $success = false;
+            $message = "Erreur : la modification n'a pas pu être réalisée.";
+        }
+
+        return redirect()->route('films.index')->with([
+            'success' => $success,
+            'message' => $message
         ]);
-        $film = Film::findOrFail($id);
-        $film->titre = $request->input('titre');
-        $film->date_sortie = $request->input('date_sortie');
-        $film->synopsis = $request->input('synopsis');
-        $film->duree = $request->input('duree');
-        $film->note = $request->input('note');
-        $film->media = $request->input('media');
-        $film->save();
-
-        return redirect()->route('films.index')->with('success', 'Film mis à jour avec succès.');
     }
+
 
     public function show($id){
         $film = Film::findOrFail($id);
