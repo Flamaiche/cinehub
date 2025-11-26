688f99e2d6561d4c30e8202879bcd80776eedaa7
Jabir Danoun <jabir_danoun@univ-artois.fr>
2025-11-26 09:56:24 +0100

    Realisation de l'envoi de l’email de réinitialisation du mot de passe (question 1.7 ticket #16)

--- app/Notifications/CustomResetPasswordNotification.php
+++ app/Notifications/CustomResetPasswordNotification.php
@@ -3,13 +3,26 @@
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
-class CustomResetPasswordNotification extends ResetPassword {
-
-    public function toMail($notifiable) {
-        $url = url('password.reset', $this->token, 'email' => $notifiable->getEmailForPasswordReset());
-
-
+class CustomResetPasswordNotification extends ResetPassword
+{
+    public function __construct($token)
+    {
+        parent::__construct($token);
     }
 
+    public function toMail($notifiable)
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Réinitialisation de votre mot de passe')
+            ->line('Vous recevez cet email parce que nous avons reçu une demande de réinitialisation de mot de passe pour votre compte.')
+            ->action('Réinitialiser le mot de passe', $url)
+            ->line('Si vous n’êtes pas à l’origine de cette demande, aucune autre action n’est requise.');
+    }
 }
