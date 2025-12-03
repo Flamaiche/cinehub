b22bc16668d9b356c441d687c53af097f06c47a5
malik_babahamou <malik_babahamou@ens.univ-artois.fr>
2025-12-03 10:22:31 +0100

    Oopss, forgot description column

--- database/migrations/2025_12_03_100621_create_media_table.php
+++ database/migrations/2025_12_03_100621_create_media_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('type')->comment('image, video');
             $table->string('url');
+            $table->string('description');
         });
     }
 
