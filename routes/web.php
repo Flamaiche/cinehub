55da312515579f4c40873c9398309af0815abbda
malik_babahamou <malik_babahamou@ens.univ-artois.fr>
2025-12-10 10:09:19 +0100

    ajust route to avoid conflict between our 2 route

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,55 @@
 <?php
 
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\MediaController;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-
 Route::view('/', 'home')->name('home');
-
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/presentation', 'presentation')->name('presentation');
 
-
 Route::get('films', [FilmController::class, 'index'])->name('films.index');
 
-Route::middleware(['auth', 'role:user,admin'])->group(function () {
-    Route::get('films/{film}', [FilmController::class, 'show'])->name('films.show');
-});
-
+// Routes admin films : AVANT le wildcard {film}
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('films/create', [FilmController::class, 'create'])->name('films.create');
     Route::post('films', [FilmController::class, 'store'])->name('films.store');
     Route::get('films/{film}/edit', [FilmController::class, 'edit'])->name('films.edit');
     Route::put('films/{film}', [FilmController::class, 'update'])->name('films.update');
     Route::delete('films/{film}', [FilmController::class, 'destroy'])->name('films.destroy');
+
+    // Acteurs
+    Route::post('/films/{film}/acteurs', [FilmController::class, 'attachActor'])
+        ->name('films.attachActor');
+    Route::put('/films/{film}/acteurs/{acteur}', [FilmController::class, 'updateActor'])
+        ->name('films.updateActor');
+    Route::delete('/films/{film}/acteurs/{acteur}', [FilmController::class, 'detachActor'])
+        ->name('films.detachActor');
+
+    // Médias
+    Route::post('/medias/upload', [MediaController::class, 'upload'])->name('medias.upload');
+    Route::delete('/medias/{id}', [MediaController::class, 'delete'])->name('medias.delete');
+});
+
+// Route wildcard {film} : APRÈS les routes spécifiques
+Route::middleware(['auth', 'role:user,admin'])->group(function () {
+    Route::get('films/{film}', [FilmController::class, 'show'])->name('films.show');
 });
 
 Route::get('/test-auth', function () {
     if (Auth::check()) {
-        return 'Vous êtes connecté en tant que : ' . Auth::user()->email;
+        return 'Vous êtes connecté en tant que : '.Auth::user()->email;
     }
     return 'Vous n\'êtes pas connecté';
 });
 
 Route::get('/logout-form', function () {
     return '<form method="POST" action="/logout">
-                ' . csrf_field() . '
+                '.csrf_field().'
                 <button type="submit">Se déconnecter</button>
             </form>';
 });
@@ -48,32 +60,15 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/profil', [ProfilController::class, 'update'])->name('profil.update');
     Route::patch('/profil/password', [ProfilController::class, 'updatePassword'])->name('profil.password.update');
     Route::delete('/profil', [ProfilController::class, 'destroy'])->name('profil.destroy');
-
-    // ↓↓↓ AJOUTS MINIMAUX POUR LES ACTEURS ↓↓↓
-    Route::post('/films/{film}/acteurs', [FilmController::class, 'attachActor'])
-        ->name('films.attachActor');
-
-    Route::put('/films/{film}/acteurs/{acteur}', [FilmController::class, 'updateActor'])
-        ->name('films.updateActor');
-
-    Route::delete('/films/{film}/acteurs/{acteur}', [FilmController::class, 'detachActor'])
-        ->name('films.detachActor');
-    // ↑↑↑ FIN DES AJOUTS ↑↑↑
-
-    Route::post('/medias/upload', [MediaController::class, 'upload'])->name('medias.upload');
-    Route::delete('/medias/{id}', [MediaController::class, 'delete'])->name('medias.delete');
 });
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
-
     Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
         ->name('password.email');
-
     Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
         ->name('password.reset');
-
     Route::post('/reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 });
